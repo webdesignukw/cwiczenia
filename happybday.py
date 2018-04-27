84357c8231c357794f1c622a1ab0376ac7d06f51
zuza sugier <webdesignukwh20@gmail.com>
2018-04-27 09:32:22 +0200

    COMMIT

---
+++ happybday.py
@@ -0,0 +1,12 @@
+def hpybday(nazwa):
+    print("happy bday to u")
+    print("happy bday to u")
+    print("happy bday, dear " + nazwa + ".")
+    print("happy bday to u")
+
+
+def cokolwiek():
+    nazwausera = input ("Podaj imie")
+    hpybday(nazwausera)
+
+cokolwiek()
---
+++ test1.py
@@ -0,0 +1,7 @@
+def potega3(x):
+    return x*x*x
+
+def main():
+    print (potega3(2))
+
+main()
